64d709e5cc75311b3beb378f5d47af60a84d5843
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2017-02-13 00:51:59 -0200

    evdev_libudev.c: Map ABS to KEY events, and dispatch to uinput

--- userspace/evdev_libudev.c
+++ userspace/evdev_libudev.c
@@ -1,5 +1,6 @@
 #include <libudev.h>
 #include <libevdev/libevdev.h>
+#include <libevdev/libevdev-uinput.h>
 
 #include <stdio.h>
 #include <string.h>
@@ -15,8 +16,9 @@ int main()
 	struct udev_device *dev;
 	const char *node;
 
-	struct libevdev *ev= NULL;
-	int fd, rc = 1;
+	struct libevdev *ev= NULL, *uev = NULL;
+	struct libevdev_uinput *uinput_ev = NULL;
+	int fd, rc = 1, ufd;
 
 	udev = udev_new();
 	if (!udev) {
@@ -68,6 +70,22 @@ int main()
 		return 1;
 	}
 
+	ufd = open("/dev/uinput", O_RDWR);
+	if (fd == -1) {
+		perror("open uinput");
+		return 1;
+	}
+
+	uev = libevdev_new();
+	libevdev_set_name(uev, "input_mapper");
+	libevdev_enable_event_code(uev, EV_KEY, KEY_LEFT, NULL);
+	libevdev_enable_event_code(uev, EV_KEY, KEY_RIGHT, NULL);
+
+	if (libevdev_uinput_create_from_device(uev, ufd, &uinput_ev)) {
+		fprintf(stderr, "uinput could not be created\n");
+		return 1;
+	}
+
 	do {
 		struct input_event iev;
 		rc = libevdev_next_event(ev, LIBEVDEV_READ_FLAG_NORMAL, &iev);
@@ -79,6 +97,16 @@ int main()
 				, libevdev_event_type_get_name(iev.type)
 				, libevdev_event_code_get_name(iev.type, iev.code)
 				, iev.value);
+
+		// redirect event to uinput, and convert to arrow keys
+		if (iev.type == EV_ABS && iev.code == ABS_X && (iev.value == 0 || iev.value == 255)) {
+			int key = iev.value == 0 ? KEY_LEFT : KEY_RIGHT;
+
+			libevdev_uinput_write_event(uinput_ev, EV_KEY, key, 1);
+			libevdev_uinput_write_event(uinput_ev, EV_SYN, SYN_REPORT, 0);
+			libevdev_uinput_write_event(uinput_ev, EV_KEY, key, 0);
+			libevdev_uinput_write_event(uinput_ev, EV_SYN, SYN_REPORT, 0);
+		}
 	} while (rc == 0 || rc == 1 || rc == -EAGAIN);
 
 	udev_enumerate_unref(enumerate);
