fed6db7ce1ef04cd4dc46ce2a2ac804b857beb6a
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2017-09-26 23:57:38 -0300

    Add new namespaces dir and a clone/namespaces example
    Also, move caps and user example to namespaces
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

---
+++ userspace/namespaces/clone.c
@@ -0,0 +1,64 @@
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sched.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define STACK_SIZE (1024 * 1024)
+static char child_stack[STACK_SIZE];
+
+static int childFunc(void *arg)
+{
+	(void)arg;
+	printf("Child inside namespace\n");
+	printf("\tPID: %d, UID: %d, GID: %d\n", getpid(), getuid(), getgid());
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+	int clone_user = 0;
+	int clone_pid = 0;
+	int clone_flags = SIGCHLD;
+	int c;
+
+	while ((c = getopt(argc, argv, "up")) != -1) {
+		switch (c) {
+		case 'u':
+			clone_user = 1;
+			break;
+		case 'p':
+			clone_pid = 1;
+			break;
+		default:
+			fprintf(stderr, "Usage: clone <-p> <-u>\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (clone_user)
+		clone_flags |= CLONE_NEWUSER;
+	if (clone_pid)
+		clone_flags |= CLONE_NEWPID;
+
+	pid = clone(childFunc, child_stack + STACK_SIZE /* stack growsdownward */
+			, clone_flags, NULL);
+
+	if (pid == -1) {
+		perror("clone");
+		exit(EXIT_FAILURE);
+	}
+
+	if (waitpid(pid, NULL, 0) == -1) {
+		perror("waitpid");
+		exit(EXIT_FAILURE);
+	}
+
+	return 0;
+}
