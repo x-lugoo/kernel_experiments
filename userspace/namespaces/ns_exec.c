a6d7ff4369fe4c23800d6e9f191c40b509a8a965
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2017-10-09 23:43:33 -0300

    Add example of ns_exec, and make MS_SLAV or /proc
    Fedora seems to have an "always propagate mount" policy, so this piece
    is needed[1].
    
    [1] https://lwn.net/Articles/635825/
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

---
+++ userspace/namespaces/ns_exec.c
@@ -0,0 +1,80 @@
+#define _GNU_SOURCE
+
+#include <getopt.h>
+#include <sched.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/mount.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <helper.h>
+
+#define STACK_SIZE (1024 * 1024)
+static char child_stack[STACK_SIZE];
+
+static int child_func(void *arg)
+{
+	int flags = *(int *)arg;
+
+	printf("Child PID: %d\n", getpid());
+	if (flags & CLONE_NEWNS) {
+		if (mount(NULL, "/proc", NULL, MS_SLAVE, NULL) < 0)
+			fatalErr("mount slave");
+		if (mount("proc", "/proc", "proc", 0, NULL) < 0)
+			fatalErr("mount proc");
+		printf("/proc was made slave and remounted\n");
+	}
+
+	if (execlp("/bin/bash", "bash", NULL) == -1)
+		fatalErr("execlp");
+
+	return 0;
+}
+
+int main(int argc, char **argv)
+{
+	pid_t pid;
+	int flags = SIGCHLD;
+	int opt;
+
+	while ((opt = getopt(argc, argv, "inmpuU")) != -1) {
+		switch (opt) {
+		case 'i':
+			flags |= CLONE_NEWIPC;
+			break;
+		case 'n':
+			flags |= CLONE_NEWNET;
+			break;
+		case 'm':
+			flags |= CLONE_NEWNS;
+			break;
+		case 'p':
+			flags |= CLONE_NEWPID;
+			break;
+		case 'u':
+			flags |= CLONE_NEWUTS;
+			break;
+		case 'U':
+			flags |= CLONE_NEWUSER;
+			break;
+		default:
+			fprintf(stderr, "Usage: %s <-i for ipcns> <-n for"
+					"netns> <-m for newns> <-p for pidns>"
+					" <-u for newuts> ,-U for userns\n"
+					, argv[0]);
+		}
+	}
+
+	/* stack grows downward */
+	pid = clone(child_func, child_stack + STACK_SIZE, flags
+			, (void *)&flags);
+	if (pid == -1)
+		fatalErr("clone");
+
+	if (waitpid(pid, NULL, 0) == -1)
+		fatalErr("waitpid");
+
+	return 0;
+}
