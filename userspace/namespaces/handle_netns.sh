82e66b9185116f8e3002aa006ab36cabe551a448
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-01-10 04:55:05 -0200

    Trying harder to have a new netns with internet access...
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- userspace/namespaces/handle_netns.sh
+++ userspace/namespaces/handle_netns.sh
@@ -1,34 +1,48 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ];
-then
-	echo "Usage: $0 <create|delete>"
+# References of iptables:
+#	https://gist.github.com/dpino/6c0dca1742093346461e11aa8f608a99
+
+set -x
+
+if [[ $EUID -ne 0 ]]; then
+	echo "You need to be root! Aborting"
 	exit 1
 fi
 
-if [ "$1" = "create" ];
-then
-	ip netns add netns1
-	ip link add veth1 type veth peer name veth2
-	ip link set veth2 netns netns1
+NS="guest"
+VH_ADDR="10.200.1.1"
+VP_ADDR="10.200.1.2"
 
-	ip link add bridge0 type bridge
-	ip addr add 172.17.42.1/24 dev bridge0
-	ip link set bridge0 up
-	ip link set veth1 master bridge0
-	ip link set veth1 up
+ip link delete vHOST &>/dev/null
+ip netns delete $NS &>/dev/null
 
-	ip netns exec netns1 ip link set veth2 name eth0
-	ip netns exec netns1 ip link set eth0 up
-	ip netns exec netns1 ip addr add 172.17.42.100/24 dev eth0
-	ip netns exec netns1 ip route add default via 172.17.42.1
-fi
+ip netns add $NS
+ip link add vHOST type veth peer name vPEER
+ip link set vPEER netns $NS
 
-if [ "$1" = "delete" ];
-then
-	ip link delete veth1
-	ip link delete bridge0
-	ip netns delete netns1
-fi
+ip addr add $VH_ADDR/24 dev vHOST
+ip link set vHOST up
 
-exit 0
+ip netns exec $NS ip link set vPEER name eth0
+ip netns exec $NS ip link set eth0 up
+ip netns exec $NS ip addr add $VP_ADDR/24 dev eth0
+ip netns exec $NS ip route add default via $VH_ADDR
+
+# enable ip forward
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
+# Flush forward rules
+iptables -P FORWARD DROP
+iptables -F FORWARD
+
+# Flush nat rules
+iptables -t nat -F
+
+# Add masquerading to host veth
+iptables -t nat -A POSTROUTING -s $VH_ADDR/24 -o enp0s8 -j MASQUERADE
+iptables -A FORWARD -i enp0s8 -o $VH_ADDR -j ACCEPT
+iptables -A FORWARD -o enp0s8 -i $VH_ADDR -j ACCEPT
+
+# execute bash inside the "guest" network namespace
+ip netns exec $NS bash
