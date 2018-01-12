145bb3fbcd7e5673cb79e9c1f147da2cde7c9882
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-01-11 23:33:38 -0200

    handle_netns.sh: Add proper iptables MASQUERADE and forward rules
    Now we can finally reach the outside world be fixing the routing to the
    physical layer.
    
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- userspace/namespaces/handle_netns.sh
+++ userspace/namespaces/handle_netns.sh
@@ -2,17 +2,24 @@
 
 # References of iptables:
 #	https://gist.github.com/dpino/6c0dca1742093346461e11aa8f608a99
-
-set -x
+#	https://blog.yadutaf.fr/2014/01/19/introduction-to-linux-namespaces-part-5-net/
+#	https://bbs.archlinux.org/viewtopic.php?id=114020
 
 if [[ $EUID -ne 0 ]]; then
 	echo "You need to be root! Aborting"
 	exit 1
 fi
 
+if [ $# -ne 1 ]; then
+	echo "Usage: handle_netns.sh <if>"
+	exit 1
+fi
+
+HOST_IF=$1
+
 NS="guest"
-VH_ADDR="10.200.1.1"
-VP_ADDR="10.200.1.2"
+VH_ADDR="10.10.1.1"
+VP_ADDR="10.10.1.2"
 
 ip link delete vHOST &>/dev/null
 ip netns delete $NS &>/dev/null
@@ -32,17 +39,10 @@ ip netns exec $NS ip route add default via $VH_ADDR
 # enable ip forward
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-# Flush forward rules
-iptables -P FORWARD DROP
-iptables -F FORWARD
-
-# Flush nat rules
-iptables -t nat -F
-
 # Add masquerading to host veth
-iptables -t nat -A POSTROUTING -s $VH_ADDR/24 -o enp0s8 -j MASQUERADE
-iptables -A FORWARD -i enp0s8 -o $VH_ADDR -j ACCEPT
-iptables -A FORWARD -o enp0s8 -i $VH_ADDR -j ACCEPT
+iptables -t nat -A POSTROUTING -j MASQUERADE
+iptables -A FORWARD -i $HOST_IF -o vHOST -j ACCEPT
+iptables -A FORWARD -i vHOST -o $HOST_IF -j ACCEPT
 
 # execute bash inside the "guest" network namespace
 ip netns exec $NS bash
