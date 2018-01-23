40cae846f446bc92418f54b99e48022d085c3adb
Marcos Paulo de Souza <marcos.souza.org@gmail.com>
2018-01-23 00:08:22 -0200

    writable_cgroups.sh: Add argument to specify name of the cgroup
    Signed-off-by: Marcos Paulo de Souza <marcos.souza.org@gmail.com>

--- userspace/namespaces/writable_cgroups.sh
+++ userspace/namespaces/writable_cgroups.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <hierarchi name>"
+	exit 1
+fi
+
+HIE_NAME=$1
+
 printf '\n\033[42mCreating cgroup hierarchy\033[m\n\n' &&
 for d in /sys/fs/cgroup/*; do
 	f=$(basename "$d")
@@ -9,8 +16,9 @@ for d in /sys/fs/cgroup/*; do
 	elif [ "$f" = "memory" ]; then
 		echo 1 | sudo tee -a "$d/memory.use_hierarchy;"
 	fi
-	sudo mkdir -p "$d/$USER"
-	sudo chown -R "$USER" "$d/$USER"
+	DIRN="$d/$HIE_NAME"
+	sudo mkdir -p "$DIRN"
+	sudo chown -R "$USER" "$DIRN"
 	# add current process to cgroup
-	echo $PPID > "$d/$USER/tasks"
+	echo $PPID > "$DIRN/tasks"
 done
